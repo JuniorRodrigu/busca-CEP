62ecfc14b1a235f04363d21cb8ef82030728265f
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-12-24 02:09:33 -0300

    teste

--- main.js
+++ main.js
@@ -25,3 +25,15 @@ document.querySelectorAll(".text-input").forEach((element) => {
       })
     });
       }
+      const $html = document.querySelector('html')
+      const $checkbox = document.querySelector('#switch')
+
+      $checkbox.addEventListener('change', function(){
+        $html.classList.toggle('dark-mode')
+      })
+      
+
+
+
+
+      
